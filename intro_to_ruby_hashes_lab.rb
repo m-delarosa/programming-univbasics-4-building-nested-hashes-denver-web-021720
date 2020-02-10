8b8ebcb7b15c198aa59c02452b110d8e6006f0b1
Michael De La Rosa <mdelarosa86@gmail.com>
2020-02-10 00:50:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,14 +25,14 @@ def monopoly_with_third_tier
   # complexity.
     monopoly = {
 	  railroads: {
-	    pieces: 4,
+	    pieces: 4
 	    rent_in_dollars: {
 	      key1: 0,
 	      key2: 0,
 	      key3: 0,
 	      key4: 0
 	    }
-	    names: 0,
+	    names: 0
 	  },
 	}
 end
