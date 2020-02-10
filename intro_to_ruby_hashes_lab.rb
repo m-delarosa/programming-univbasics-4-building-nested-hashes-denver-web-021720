67932fb06098022704c2ec0fec29f79a67b21a8d
Michael De La Rosa <mdelarosa86@gmail.com>
2020-02-10 00:53:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ def monopoly_with_third_tier
 	  railroads: {
 	    pieces: 4,
 	    rent_in_dollars: {
-	      key1: 0,
+	      one_piece_owned: 25,
 	      key2: 0,
 	      key3: 0,
 	      key4: 0
